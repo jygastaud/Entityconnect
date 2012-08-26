9852c71a60a484d20031b4a34c43d383d64774f5
Jean yves GASTAUD <jygastaud@addvista.fr>
2012-08-26 23:15:15 +0200

    [#1748440] Allow editing with widgets: radio and select when value is unique. [#1722926] Solved issue when body field is not displayed.

--- theme/js/entityconnect.js
+++ theme/js/entityconnect.js
@@ -5,10 +5,10 @@
 
       // Treatments for each widget type.
       // Autocomplete widget.
-      $(".entityconnect-add.single-value", context).each(function() {
+      $(".entityconnect-add.autocomplete.single-value", context).each(function() {
         $(this).insertAfter($(this).next().find("label"));
       });
-      $(".entityconnect-edit.single-value", context).each(function() {
+      $(".entityconnect-edit.autocomplete.single-value", context).each(function() {
         $(this).insertAfter($(this).next().find("label"));
       });
 
@@ -16,15 +16,27 @@
       $(".entityconnect-add.textfield", context).each(function() {
         $(this).insertAfter($(this).next().find("label"));
       });
+      $(".entityconnect-edit.textfield", context).each(function() {
+        $(this).insertAfter($(this).next().find("label"));
+      });
       // Select widget.
       $(".entityconnect-add.select", context).each(function() {
         $(this).insertAfter($(this).next().find("label"));
       });
+      $(".entityconnect-edit.select.single-value", context).each(function() {
+        $(this).insertAfter($(this).next().find("label"));
+      });
       // Radios widget.
+      $(".entityconnect-edit.radios", context).each(function() {
+        $(this).insertBefore($(this).siblings("div.form-type-radios").find("label").first());
+      });
       $(".entityconnect-add.radios", context).each(function() {
         $(this).insertBefore($(this).siblings("div.form-type-radios").find("label").first());
       });
       // Checkboxes widget.
+      $(".entityconnect-edit.checkboxes", context).each(function() {
+        $(this).insertBefore($(this).siblings("div.form-type-checkboxes").find("label").first());
+      });
       $(".entityconnect-add.checkboxes", context).each(function() {
         $(this).insertBefore($(this).siblings("div.form-type-checkboxes").find("label").first());
       });
@@ -35,12 +47,21 @@
         var wrapper = $(this).parents(".entityconnect-edit");
 
         text = $(wrapper).siblings("[type='text']");
+        radio = $(wrapper).siblings("[type='radio']");
+        select = $(wrapper).siblings("select");
 
         if(text.length == 0) {
           text = $(wrapper).siblings().find("[type='text']");
         }
+        if(radio.length == 0) {
+          radio = $(wrapper).siblings().find("[type='radio']");
+        }
+        if(select.length == 0) {
+          select = $(wrapper).siblings().find("select > option");
+        }
 
-        if($.trim($(text).val()) == '') {
+        if($.trim($(text).val()) == '' && $.trim($(radio).val()) == ''
+             && $.trim($(select).val()) == '') {
           return false;
         }
         return true;
