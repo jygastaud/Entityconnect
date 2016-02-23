74b9a00a355589dd3dfe52e2ed4a4659565f12f0
Agnes Chisholm <amaria@chisholmtech.com>
2016-02-23 09:24:01 -0500

    Fixes improper spacing between t() array elements.

--- src/Form/AdministrationForm.php
+++ src/Form/AdministrationForm.php
@@ -96,65 +96,65 @@ class AdministrationForm extends ConfigFormBase {
       '#weight' => '0',
       '#type' => 'radios',
       '#options' => array(
-        '0' =>t('on'),
-        '1' =>t('off'),
+        '0' => t('on'),
+        '1' => t('off'),
       ),
-      '#title' =>t('Default Entity Connect "add" button display'),
+      '#title' => t('Default Entity Connect "add" button display'),
     );
 
     $form['entityconnect']['buttons']['button_edit'] = array(
       '#required' => '1',
       '#default_value' => $defaults['buttons']['button_edit'],
-      '#description' =>t('Default: "off"<br />
+      '#description' => t('Default: "off"<br />
                             Choose "on" if you want the "edit" buttons displayed by default.<br />
                             Each field can override this value.'),
       '#weight' => '1',
       '#type' => 'radios',
       '#options' => array(
-        '0' =>t('on'),
-        '1' =>t('off'),
+        '0' => t('on'),
+        '1' => t('off'),
       ),
-      '#title' =>t('Default Entity Connect "edit" button display'),
+      '#title' => t('Default Entity Connect "edit" button display'),
     );
 
     $form['entityconnect']['icons'] = array(
       '#type' => 'fieldset',
-      '#title' =>t('Icons display Parameters'),
+      '#title' => t('Icons display Parameters'),
     );
 
     $form['entityconnect']['icons']['icon_add'] = array(
       '#required' => '1',
       '#key_type_toggled' => '1',
       '#default_value' => $defaults['icons']['icon_add'],
-      '#description' =>t('Default: "Icon only"<br />
+      '#description' => t('Default: "Icon only"<br />
                            Choose "Icon + Text" if you want to see the edit (pencil) icon + the text displayed by default.<br />
                            Choose "Text only" if you don\'t want to see the edit (pencil) icon displayed by default.<br />
                            Each field can override this value.'),
       '#weight' => '2',
       '#type' => 'radios',
       '#options' => array(
-        '0' =>t('Icon only'),
-        '1' =>t('Icon + Text'),
-        '2' =>t('Text only')
+        '0' => t('Icon only'),
+        '1' => t('Icon + Text'),
+        '2' => t('Text only')
       ),
-      '#title' =>t('Default Entity Connect "add (+) icon" display'),
+      '#title' => t('Default Entity Connect "add (+) icon" display'),
     );
 
     $form['entityconnect']['icons']['icon_edit'] = array(
       '#required' => '1',
       '#default_value' => $defaults['icons']['icon_edit'],
-      '#description' =>t('Default: "Icon only"<br />
+      '#description' => t('Default: "Icon only"<br />
                            Choose "Icon + Text" if you want to see the edit (pencil) icon + the text displayed by default.<br />
                            Choose "Text only" if you don\'t want to see the edit (pencil) icon displayed by default.<br />
                            Each field can override this value.'),
       '#weight' => '3',
       '#type' => 'radios',
       '#options' => array(
-        '0' =>t('Icon only'),
-        '1' =>t('Icon + Text'),
-        '2' =>t('Text only')
+        '0' => t('Icon only'),
+        '1' => t('Icon + Text'),
+        '2' => t('Text only')
       ),
-      '#title' =>t('Default Entity Connect "edit (pencil) icon" display'),
+      '#title' => t('Default Entity Connect "edit (pencil) icon" display'),
     );
   }
 }
